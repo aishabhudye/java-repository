d23b4687b5f62157df33936777bf5faaa43a2c62
Aisha Bhudye <aisha.bhudye@gmail.com>
2022-07-27 10:53:12 +0100

    My first lines of Java code

--- complete/src/main/java/com/example/iteration/SimpleArrays.java
+++ complete/src/main/java/com/example/iteration/SimpleArrays.java
@@ -11,7 +11,8 @@ public class SimpleArrays {
 
     private void runCode() {
         String[] siblings = {"Aisha", "Hafsa", "Talha"};
-        for (int i=0; i< siblings.length; i++){
+        int i;
+        for (i = 0; i < siblings.length; i++) {
             System.out.println("Loop over array with index, child name is " + siblings[i]);
         }
 
@@ -25,6 +26,14 @@ public class SimpleArrays {
         System.out.println("==============================");
         System.out.println();
 
+
+        int z = 0;
+        while (z < siblings.length) {
+            System.out.println(siblings[z]);
+            z++;
+        }
+
+
         List<String> children = new ArrayList<>();
         children.add("Aisha");
         children.add("Hafsa");
@@ -33,6 +42,28 @@ public class SimpleArrays {
             System.out.println("Loop over list without index, child name is " + child);
         }
 
-    }
 
+        System.out.println();
+        {
+            List<String> kids= new ArrayList<>();
+            kids.add("Aisha");
+            kids.add("Hafsa");
+            kids.add("Talha");
+
+            System.out.println("List: " + kids);
+            String element = kids.get(0);
+            System.out.println("the element at index 0 is "
+                    + element);
+
+            String[] things = {"Aisha", "Hafsa", "Talha"};
+            int y;
+            for (y= 0; y < things.length; y++) {
+                System.out.println("Child at index " + y + " is " + things[y]);
+            }
+
+
+        }
+    }
 }
+
+
