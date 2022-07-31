b7d25a44fd92ff7b6abeb3b22444d317a76523ae
Aisha Bhudye <aisha.bhudye@gmail.com>
2022-07-31 14:00:52 +0100

    A method to reverse an array of int primitives

--- complete/src/main/java/com/example/iteration/IterationExercises.java
+++ complete/src/main/java/com/example/iteration/IterationExercises.java
@@ -1,15 +1,40 @@
 package com.example.iteration;
 
+import java.lang.reflect.Array;
+
 public class IterationExercises {
 
-    public String[] reverseArray(String[] stringArray){
+    public String[] reverseArray(String[] siblings) {
+        System.out.println(siblings);
+        String[] reversedSiblings = new String[siblings.length];
+        for (int i = 0; i < siblings.length; i++) {
+            System.out.println("Loop over array with index, child name is " + siblings[i]);
+            reversedSiblings[siblings.length - (i + 1)] = siblings[i];
+        }
 
-        //TODO:
-        // Print the incoming array
-        // Create a new array of the same length as the incoming array
-        // While iterating over the incoming array, populate the new new array in reverse order
-        // Return the new array
+        return reversedSiblings;
+    }
 
-        return null;
+    public String[] reverseLetters(String[] alphabet) {
+        System.out.println(alphabet);
+        String[] reversedAlphabet = new String[alphabet.length];
+        for (int i = 0; i < alphabet.length; i++) {
+            System.out.println("Loop over array with index, child name is " + alphabet[i]);
+            reversedAlphabet[alphabet.length - (i + 1)] = alphabet[i];
+        }
+
+        return reversedAlphabet;
+    }
+
+    public int[] reverseIntegers(int[] numbers) {
+        System.out.println(numbers);
+        int[] reversedNumbers = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Loop over array with index, child name is " + numbers[i]);
+            reversedNumbers[numbers.length - (i + 1)] = numbers[i];
+        }
+
+        return reversedNumbers;
     }
 }
+    
--- complete/src/test/java/com/example/iteration/IterationExercisesTest.java
+++ complete/src/test/java/com/example/iteration/IterationExercisesTest.java
@@ -2,6 +2,8 @@ package com.example.iteration;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class IterationExercisesTest {
@@ -14,5 +16,24 @@ class IterationExercisesTest {
         String[] reversedSiblings = {"Talha", "Hafsa", "Aisha"};
         String[] reversedArray = exercises.reverseArray(siblings);
         assertArrayEquals(reversedSiblings, reversedArray);
+
+    }
+
+    @Test
+    void test_reverse_alphabet(){
+        String[] alphabet = {"a","b","c","d","e","f","g","h","i","j",};
+        String[] reversedAlphabet = {"j","i","h","g","f","e","d","c","b","a"};
+        String[] reversedLetters = exercises.reverseArray(alphabet);
+        assertArrayEquals(reversedAlphabet, reversedLetters);
+
+    }
+
+    @Test
+    void test_reverse_numbers(){
+        int[] numbers = {10,20,30,40,50};
+        int[] reversedNumbers = {50,40,30,20,10};
+        int[] reversedIntegers = exercises.reverseIntegers(numbers);
+        assertArrayEquals(reversedNumbers, reversedIntegers);
+
     }
 }
