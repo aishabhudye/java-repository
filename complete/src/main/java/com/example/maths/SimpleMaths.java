3db619bdb20da3f7b7cc83b58615c85da0a0a92a
zulbhudy <zabdsb@gmail.com>
2022-08-03 11:43:05 +0100

    Next set of exercises focuses on Maths problems

---
+++ complete/src/main/java/com/example/maths/SimpleMaths.java
@@ -0,0 +1,21 @@
+package com.example.maths;
+
+public class SimpleMaths {
+    public int[] listEvenNumbers(int limit) {
+        //Use a loop
+        //Lookup the mod operation in Java
+        return new int[0];
+    }
+
+    public int[] listOddNumbers(int limit) {
+        //Use a loop
+        //Lookup the mod or div operation in Java
+        return new int[0];
+    }
+
+    public int[] listTriangularNumbers(int i) {
+        //Use a loop
+        //Google a general formula for triangular numbers
+        return new int[0];
+    }
+}
---
+++ complete/src/test/java/com/example/maths/SimpleMathsTest.java
@@ -0,0 +1,32 @@
+package com.example.maths;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class SimpleMathsTest {
+
+    private final SimpleMaths simpleMaths = new SimpleMaths();
+
+    @Test
+    public void test_list_even_numbers(){
+        int[] evenNumbers = simpleMaths.listEvenNumbers(10);
+        int[] expectedEvenNumbers = {0, 2, 4, 6, 8, 10};
+        assertArrayEquals(expectedEvenNumbers, evenNumbers);
+    }
+
+    @Test
+    public void test_list_odd_numbers(){
+        int[] oddNumbersNumbers = simpleMaths.listOddNumbers(10);
+        int[] expectedOddNumbers = {1, 3, 5, 7, 9};
+        assertArrayEquals(expectedOddNumbers, oddNumbersNumbers);
+    }
+
+    @Test
+    public void test_list_triangular_numbers(){
+        int[] triangularNumbers = simpleMaths.listTriangularNumbers(10);
+        int[] expectedTriangularNumbers = {1, 3, 6, 10};
+        assertArrayEquals(expectedTriangularNumbers, triangularNumbers);
+    }
+
+}
