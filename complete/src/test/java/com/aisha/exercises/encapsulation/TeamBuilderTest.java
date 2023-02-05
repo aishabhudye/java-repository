8ca39171d1a129bf8319e4a1d72916a7b867f3e7
aishabhudye <aisha.bhudye@gmail.com>
2023-02-05 17:36:58 +0000

    Tests

--- complete/src/test/java/com/aisha/exercises/encapsulation/TeamBuilderTest.java
+++ complete/src/test/java/com/aisha/exercises/encapsulation/TeamBuilderTest.java
@@ -2,6 +2,9 @@ package com.aisha.exercises.encapsulation;
 
 import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 class TeamBuilderTest {
     private final TeamBuilder teamBuilder = new TeamBuilder();
@@ -10,5 +13,22 @@ class TeamBuilderTest {
     public void testTeamNames(){
         //assert equals("Wolverhampton Wanderers",teamBuilder.buildTeams(""))
     }
+    @Test
+    public void testListSize(){
+        String path = "C:\\Users\\Aisha\\IdeaProjects\\java-repository\\complete\\src\\main\\resources\\PremierLeagueTableUnsorted.csv";
+        List<Team> teams = teamBuilder.build(path);
+        assertEquals(20,teams.size());
+
+    }
+    @Test
+    public int testTeamProperties(){
+        String path = "C:\\Users\\Aisha\\IdeaProjects\\java-repository\\complete\\src\\main\\resources\\PremierLeagueTableUnsorted.csv";
+        Team Chelsea = (Team) teamBuilder.build(path);
+        assertEquals(20,Chelsea.getPlayed());
+
+        return Chelsea.getPlayed();
+    }
+
+
 
 }
