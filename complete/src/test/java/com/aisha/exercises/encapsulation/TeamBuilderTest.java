586722f220ff15811d21fa7c39709dc7f4d7d923
aishabhudye <aisha.bhudye@gmail.com>
2023-02-09 10:37:19 +0000

    Tests-stable version

--- complete/src/test/java/com/aisha/exercises/encapsulation/TeamBuilderTest.java
+++ complete/src/test/java/com/aisha/exercises/encapsulation/TeamBuilderTest.java
@@ -1,23 +1,17 @@
 package com.aisha.exercises.encapsulation;
 
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader;
 import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.Test;
 
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 class TeamBuilderTest {
     private final TeamBuilder teamBuilder = new TeamBuilder();
 
-    @Ignore
-    public void testTeamNames() {
-        //assert equals("Wolverhampton Wanderers",teamBuilder.buildTeams(""))
-    }
+
 
     @Test
     public void testListSize() {
@@ -27,23 +21,72 @@ class TeamBuilderTest {
 
     }
 
-    public Team chelseaTeam() {
+    private Team retrieveTeam(String teamToSelect) {
         String path = "C:\\Users\\Aisha\\IdeaProjects\\java-repository\\complete\\src\\main\\resources\\PremierLeagueTableUnsorted.csv";
         List<Team> teams = teamBuilder.build(path);
+        Team selectedTeam = null;
         for (Team team : teams) {
-            if (team.getName().equals("Chelsea")) {
-                System.out.println("The team is"+team);
+            if (team.getName().equals(teamToSelect)) {
+                selectedTeam = team;
             }
         }
-        return teams.get(15);
+        return selectedTeam;
     }
 
     @Test
-    public void testProperties(){
+    public void testNameProperty() {
         String path = "C:\\Users\\Aisha\\IdeaProjects\\java-repository\\complete\\src\\main\\resources\\PremierLeagueTableUnsorted.csv";
-        Team expected = new Team(20,8,5,7,22,21,1,29,"Chelsea");
-        Team actual = chelseaTeam();
-        assertEquals(expected,actual);
+        Team expected = new Team(20, 8, 5, 7, 22, 21, 1, 29, "Chelsea");
+        Team actual = retrieveTeam("Chelsea");
+        assertEquals(expected.getName(), actual.getName());
+    }
+
+    @Test
+    public void testPlayedProperty() {
+        String path = "C:\\Users\\Aisha\\IdeaProjects\\java-repository\\complete\\src\\main\\resources\\PremierLeagueTableUnsorted.csv";
+        Team expected = new Team(20, 8, 5, 7, 22, 21, 1, 29, "Chelsea");
+        Team actual = retrieveTeam("Chelsea");
+        assertEquals(expected.getPlayed(), actual.getPlayed());
+    }
+
+    @Test
+    public void testWonProperty() {
+        String path = "C:\\Users\\Aisha\\IdeaProjects\\java-repository\\complete\\src\\main\\resources\\PremierLeagueTableUnsorted.csv";
+        Team expected = new Team(20, 8, 5, 7, 22, 21, 1, 29, "Chelsea");
+        Team actual = retrieveTeam("Chelsea");
+        assertEquals(expected.getWon(), actual.getWon());
+    }
+
+    @Test
+    public void testDrawnProperty() {
+        String path = "C:\\Users\\Aisha\\IdeaProjects\\java-repository\\complete\\src\\main\\resources\\PremierLeagueTableUnsorted.csv";
+        Team expected = new Team(20, 8, 5, 7, 22, 21, 1, 29, "Chelsea");
+        Team actual = retrieveTeam("Chelsea");
+        assertEquals(expected.getDrawn(), actual.getDrawn());
+    }
+
+    @Test
+    public void testLostProperty() {
+        String path = "C:\\Users\\Aisha\\IdeaProjects\\java-repository\\complete\\src\\main\\resources\\PremierLeagueTableUnsorted.csv";
+        Team expected = new Team(20, 8, 5, 7, 22, 21, 1, 29, "Chelsea");
+        Team actual = retrieveTeam("Chelsea");
+        assertEquals(expected.getLost(), actual.getLost());
+    }
+
+    @Test
+    public void testGoalsScoredProperty() {
+        String path = "C:\\Users\\Aisha\\IdeaProjects\\java-repository\\complete\\src\\main\\resources\\PremierLeagueTableUnsorted.csv";
+        Team expected = new Team(20, 8, 5, 7, 22, 21, 1, 29, "Chelsea");
+        Team actual = retrieveTeam("Chelsea");
+        assertEquals(expected.getGoalsScored(), actual.getGoalsScored());
+    }
+
+    @Test
+    public void testGoalsTakenProperty() {
+        String path = "C:\\Users\\Aisha\\IdeaProjects\\java-repository\\complete\\src\\main\\resources\\PremierLeagueTableUnsorted.csv";
+        Team expected = new Team(20, 8, 5, 7, 22, 21, 1, 29, "Chelsea");
+        Team actual = retrieveTeam("Chelsea");
+        assertEquals(expected.getGoalsTaken(), actual.getGoalsTaken());
     }
 
 
