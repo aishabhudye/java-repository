1157a2cc80d99572f31417da71ca428f56d10ea6
aishab <aisha.bhudye@gmail.com>
2023-03-02 11:18:12 +0000

    Battleship Grid 2

--- complete/src/main/java/com/aisha/exercises/games/BattleShips.java
+++ complete/src/main/java/com/aisha/exercises/games/BattleShips.java
@@ -1,9 +1,12 @@
 package com.aisha.exercises.games;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class BattleShips {
     public static void main(String[] args) {
         BattleShips battleShips = new BattleShips();
-        battleShips.printBoard();
+        battleShips.setShips();
     }
 
     public void printBoard() {
@@ -12,6 +15,27 @@ public class BattleShips {
             for (int j = 0; j < grid.length; j++) {
                 System.out.printf("|%2d ", grid[i][j]);
             }
+            System.out.println("|");
+        }
+    }
+
+    public void setShips(){
+        String[][] grid = { { " ", " ", "X", " ", " ", " ", " ", " ", " ", " " },
+                { " ", " ", " ", " ", " ", " ", " ", " ", " ", " " },
+                { " ", " ", " ", " ", " ", " ", " ", " ", " ", " " },
+                { " ", " ", " ", " ", " ", " ", " ", " ", " ", " " },
+                { " ", " ", " ", " ", " ", " ", " ", " ", " ", " " },
+                { " ", " ", " ", " ", " ", " ", " ", " ", " ", " " },
+                { " ", " ", " ", " ", " ", " ", " ", " ", " ", " " },
+                { " ", " ", " ", " ", " ", " ", " ", " ", " ", " " },
+                { " ", " ", " ", " ", " ", " ", " ", " ", " ", " " },
+                { " ", " ", " ", " ", " ", " ", " ", " ", " ", " " }};
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                System.out.print("|"+ grid[i][j]);
+            }
+
             System.out.println("|");
         }
     }
