d487b8d2268586b6ad837f7c090d9df4c37c6afe
aishab <aisha.bhudye@gmail.com>
2023-03-08 11:50:27 +0000

    Battleship Grid 8

--- complete/src/main/java/com/aisha/exercises/games/BattleShips.java
+++ complete/src/main/java/com/aisha/exercises/games/BattleShips.java
@@ -30,45 +30,54 @@ public class BattleShips {
         }
     }
 
-    public void placeShips(){
+    public void placeShips() {
         int[][] grid = {
-                {0,0,0,0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0,0,0,0}
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
         };
         int X;
         int Y;
         int size;
+        boolean horizontal;
+        boolean vertical;
+        int numberOfShips = 5;
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the size: ");
         size = input.nextInt();
-        System.out.println("Enter x value: ");
+        System.out.println("Enter the starting x value: ");
         X = input.nextInt();
-        System.out.println("Enter y value: ");
+        System.out.println("Enter the starting y value: ");
         Y = input.nextInt();
-        for (int i = 0; i<size;i++){
-            grid[X][Y + i] = Integer.parseInt("1");
+        System.out.println("Enter the orientation H or V: ");
+        horizontal = input.hasNext("H");
+        vertical = input.hasNext("V");
+        for (int i = 0; i < size; i++) {
+            if (horizontal) {
+                grid[X][Y + i] = Integer.parseInt("1");
+            } else if (vertical) {
+                grid[X + i][Y] = Integer.parseInt("1");
+            }
         }
         System.out.println("After updating an array element: ");
         printArray(grid);
     }
-    private static void printArray(int[][] grid){
-        for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid[0].length; j++){
+
+    private static void printArray(int[][] grid) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 System.out.print(grid[i][j] + "|");
             }
             System.out.println("");
         }
     }
-
-
 
 
 }
